44229286a5f68b2464c110ee3efad130ddd36718
adi501 <jc.adi101@gmail.com>
2020-01-01 19:55:36 +0530

    te

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import RouterEXP1 from "./component/RouterEXP1";
+import AxiosLoadingErrorHandling from "./component/AxiosLoadingErrorHandling";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <RouterEXP1 />
+      <AxiosLoadingErrorHandling />
     </div>
   );
 }
